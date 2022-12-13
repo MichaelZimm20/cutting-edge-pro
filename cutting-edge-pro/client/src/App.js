bb4383d2c30c12a1abd8f0b6c9529298d5c4a263
Justin Klaver <Klaverts@yahoo.com>
2022-12-13 14:29:12 -0500

    initial file added testing

--- cutting-edge-pro/client/src/App.js
+++ cutting-edge-pro/client/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Input } from 'postcss';
 
 function App() {
+
+const uploadImage = (files) => {
+  console.log(files[0]);
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+    <div>
+      <Input type='file'onChange={(event) => { 
+        uploadImage(event.target.files); 
+      }}
+        />
+    </div>
       </header>
     </div>
   );
