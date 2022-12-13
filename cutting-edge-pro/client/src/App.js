f7e377fd28c589a2776b70174e3c89e13ac64557
Justin Klaver <Klaverts@yahoo.com>
2022-12-13 15:04:18 -0500

    image uploading to cloudinary sucessfully

--- cutting-edge-pro/client/src/App.js
+++ cutting-edge-pro/client/src/App.js
@@ -1,20 +1,35 @@
+import React, { useState } from "react";
+import Axios from 'axios';
+
 import './App.css';
 
 function App() {
 
-const uploadImage = (files) => {
-  console.log(files[0]);
-}
+const [imageSelected, setImageSelected] = useState('');
+
+const uploadImage = () => {
+    const formData = new FormData()
+    formData.append("file", imageSelected)
+    formData.append('upload_preset', 'xcpvb7ai')
+
+    Axios.post('https://api.cloudinary.com/v1_1/dt1ejaaxy/image/upload', formData)
+    .then((response) => {
+      console.log(response);
+    });
+  };
+
 
 
 return (
   <div className="App">
     <header className="App-header">
       <div>
-      <input type='file'onChange={(event) => { 
-        uploadImage(event.target.files); 
+      <input type='file'
+      onChange={(event) => { 
+        setImageSelected(event.target.files[0]); 
       }}
         />
+        <button onClick={uploadImage}>Upload Image</button>
     </div>
     
     </header>
