cb4ee41fdd6a57b28a5fcf99445b2ac0b246bdae
banjosquash <banjosquash@gmail.com>
2022-12-13 08:57:43 -0500

    added user Queries in typedefs and mutations for adding user and logging in

--- cutting-edge-pro/server/schemas/typeDefs.js
+++ cutting-edge-pro/server/schemas/typeDefs.js
@@ -8,4 +8,21 @@ type User {
     password: String
     orders: [Order]
 }
-`
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    orders(username: String): [Order]
+    order(_id: ID!): Order
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addOrder(): Order
+
+}
+`;
+
+module.exports = typeDefs;
