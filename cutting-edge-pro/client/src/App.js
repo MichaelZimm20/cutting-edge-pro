811797a6455b166d6d4c9c667bd02fb946c249bf
PDismore <pat.dismore@gmail.com>
2022-12-14 20:44:23 -0500

    homepage and navbar setup

--- cutting-edge-pro/client/src/App.js
+++ cutting-edge-pro/client/src/App.js
@@ -1,50 +1,102 @@
 import React, { useState } from "react";
 import Axios from 'axios';
 import {Image} from 'cloudinary-react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 //develop test
-import './App.css';
+// import './App.css';
+import Navigation from './components/Nav';
+import Home from './pages/Home';
+import Signup from './pages/SignupPage';
+import { StoreProvider } from './utils/GlobalState';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 function App() {
-  const [imageSelected, setImageSelected] = useState('');
-  // Declare a state variable to store the image link
-  const [imageLink, setImageLink] = useState('');
+//   const [imageSelected, setImageSelected] = useState('');
+//   // Declare a state variable to store the image link
+//   const [imageLink, setImageLink] = useState('');
 
 
-const uploadImage = () => {
-  const formData = new FormData()
-  formData.append("file", imageSelected)
-  formData.append('upload_preset', 'xcpvb7ai')
+// const uploadImage = () => {
+//   const formData = new FormData()
+//   formData.append("file", imageSelected)
+//   formData.append('upload_preset', 'xcpvb7ai')
 
-  Axios.post('https://api.cloudinary.com/v1_1/dt1ejaaxy/image/upload', formData)
-  .then((response) => {
-    console.log(response);
-    const imageLink = response.data.url;
+//   Axios.post('https://api.cloudinary.com/v1_1/dt1ejaaxy/image/upload', formData)
+//   .then((response) => {
+//     console.log(response);
+//     const imageLink = response.data.url;
 
-    console.log(imageLink);
-    setImageLink(imageLink);  // Update the image link in state
-  });
-};
+//     console.log(imageLink);
+//     setImageLink(imageLink);  // Update the image link in state
+//   });
+// };
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Navigation />
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
 
-return (
-  <div className="App">
-    <header className="App-header">
-      <div>
-      <input type='file'
-      onChange={(event) => { 
-        setImageSelected(event.target.files[0]); 
-      }}
-        />
-        <button onClick={uploadImage}>Upload Image</button>
-    </div>
-    <div>
-        <Image style={{width: 200}} cloudName="dt1ejaaxy" publicId={imageLink} />
-    </div>
-    
-    </header>
-  </div>
-);
+              />
+              <Route
+              path="/signuppage"
+              element = {<Signup />}
+              />
+            {/* <div className="App">
+              <header className="App-header">
+                <div>
+                  <input type='file'
+                    onChange={(event) => {
+                      setImageSelected(event.target.files[0]);
+                    }}
+                  />
+                  <button onClick={uploadImage}>Upload Image</button>
+                </div>
+                <div>
+                  <Image style={{ width: 200 }} cloudName="dt1ejaaxy" publicId={imageLink} />
+                </div>
+
+              </header>
+            </div> */}
+            </Routes>
+          </StoreProvider>
+        </div>
+
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- cutting-edge-pro/client/src/components/Nav/index.js
+++ cutting-edge-pro/client/src/components/Nav/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function Nav() {
+function Navigation() {
 
     function showNavigation() {
       if (Auth.loggedIn()) {
@@ -24,31 +28,36 @@ function Nav() {
         );
       } else {
         return (
-          <ul className="flex-row">
-            <li className="mx-1">
-              <Link to="/signup">
-                Signup
-              </Link>
-            </li>
-            <li className="mx-1">
-              <Link to="/login">
-                Login
-              </Link>
-            </li>
-          </ul>
+          <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/signuppage">Signup</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         );
       }
+      
     }
   
     return (
       <header className="flex-row px-1">
-        <h1>
-          <Link to="/">
-            <span role="img" aria-label="shopping bag">🛍️</span>
-            -Shop-Shop
-          </Link>
-        </h1>
-  
         <nav>
           {showNavigation()}
         </nav>
@@ -56,5 +65,4 @@ function Nav() {
     );
   }
 
-
-export default Nav;
+export default Navigation;
