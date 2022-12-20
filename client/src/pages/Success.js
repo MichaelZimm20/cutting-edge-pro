a401d3d9da0e64176ef0bcd8ccbeb6c07227abfa
Justin Klaver <Klaverts@yahoo.com>
2022-12-20 16:35:16 -0500

    success centered

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function Success() {
-
-    setTimeout(() => {
-        window.location.assign('/home');
-      }, 3000);
-
+  setTimeout(() => {
+    window.location.assign('/home');
+  }, 3000);
 
   return (
-    <div>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
-    </div>
+    <Container className='text-center'>
+      <h1>Success!</h1>
+      <h2>Thank you for your purchase!</h2>
+      <h2>You will now be redirected to the home page</h2>
+    </Container>
   );
 }
 
